976e80654e39ed175b9ec5d7da1ffe7b3d5ace26
gabrll05 <142859449+gabrll05@users.noreply.github.com>
2026-03-02 17:25:55 -0600

    [FIX]: Fix global reference inside quearter_round

---
+++ src/main_block.c
@@ -0,0 +1,16 @@
+typedef unsigned int uint32_t;
+extern void chacha20_block(uint32_t *state);
+
+int main() {
+    uint32_t state[16] = {
+        0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
+        0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
+        0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
+        0x00000001, 0x09000000, 0x4a000000, 0x00000000
+    };
+
+    chacha20_block(state);
+
+    // Retornar 0 para indicar éxito
+    return 0;
+}
